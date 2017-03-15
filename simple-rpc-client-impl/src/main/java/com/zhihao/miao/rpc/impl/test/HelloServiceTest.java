47c83804d6e2206c315a29946f2ed02a26f1340d
steven <918273244@qq.com>
2017-03-15 14:01:29 +0800

    Merge branch 'dev'
    # Conflicts:
    #	simple-rpc-client-impl/src/main/java/com/zhihao/miao/rpc/impl/test/HelloServiceTest.java

--- simple-rpc-client-impl/src/main/java/com/zhihao/miao/rpc/impl/test/HelloServiceTest.java
+++ simple-rpc-client-impl/src/main/java/com/zhihao/miao/rpc/impl/test/HelloServiceTest.java
@@ -25,8 +25,6 @@ public class HelloServiceTest {
 		String result = helloService.hello("World");
 		System.out.println("服务端返回结果：");
 		System.out.println(result);
-		System.out.println("this is master");
-		System.out.println("dev 新增加");
 	}
 
 	@Test
